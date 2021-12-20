3e375248778c6e22dbb217d623e1037bda2e1628
Jojo Bautista <gjojob@yahoo.com>
2021-12-20 09:28:53 -0600

    Added delete route

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 4000;
 
-
-
+// middleware parser for incoming JSON data
 app.use(express.json());
 
-//middleware
+// static middleware for CSS, HTML and JS files
 app.use(express.static('public'));
 
 
@@ -23,16 +22,28 @@ app.get('/api/notes', (req, res) => {
         if(err) {
             res.status(500).json({msg: 'Cannot read the file}'});
         }
-        res.status(200).json(JSON.parse(noteData));
+        // Make sure that db.json is not empty to avoid error Unexpected end of JSON input
+        if(noteData){
+            res.status(200).json(JSON.parse(noteData));
+        }
     });
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// route to add note to db.json
 app.post('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, noteData) => {
+        let newNote = [];
         if(err) {
             res.status(500).json({msg: 'Cannot read the file'});
         }
-        const newNote = JSON.parse(noteData);
+        // Make sure that db.json is not empty to avoid error Unexpected end of JSON input
+        if (noteData) {
+            newNote = JSON.parse(noteData);
+        }
         req.body.id = uuidv4();
         newNote.push(req.body);
         fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(newNote), err => {
@@ -44,10 +55,26 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+// route to remove selected note
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, notes) => {
+        if(err) {
+            res.status(500).json({msg: 'Cannot read the file}'});
+        }
+        // filter out the selected note by id
+        const newNotes = JSON.parse(notes).filter(note => note.id !== req.params.id);
+   
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(newNotes), err => {
+            if (err) {
+                res.status(500).json(err.message);
+            }
+            res.status(200).end();
+        });
+    });
+
+    
 });
 
 app.listen(PORT, () => {
-    console.log(`Note taker is listening at http://localhost:${PORT}`);
+    console.log(`Note taker is running at http://localhost:${PORT}`);
 });
